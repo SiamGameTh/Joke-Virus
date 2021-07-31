2e2082ff420f3b8cea96ca03a259f66905da45f3
SiamGameTh <86415418+SiamGameTh@users.noreply.github.com>
2021-07-31 18:33:17 +0700

    Add files via upload

---
+++ loader.bat
@@ -0,0 +1,80 @@
+@echo off
+title Virus Munk
+color e
+
+set PGCLIENCODING=utf-8
+
+chcp 65001
+
+echo Downloading assets
+title Downloading assets
+
+PING localhost -n 3 >NUL
+
+echo Downloading 30%
+title Downloading 30%
+
+PING localhost -n 3 >NUL
+
+echo Downloading 40%
+title Downloading 40%
+
+PING localhost -n 3 >NUL
+
+echo Downloading 60%
+title Downloading 60%
+
+PING localhost -n 3 >NUL
+
+echo Downloading 80%
+title Downloading 80%
+
+PING localhost -n 3 >NUL
+
+echo Downloading 100%
+title Downloading 100%
+
+PING localhost -n 3 >NUL
+
+echo Process Completed 
+title Process Completed
+
+
+PING localhost -n 3 >NUL
+
+echo Please Wait .
+title Please Wait .
+cls
+PING localhost -n 3 >NUL
+
+echo Please Wait ..
+title Please Wait ..
+cls
+PING localhost -n 3 >NUL
+
+echo Please Wait ...
+title Please Wait ...
+cls
+PING localhost -n 3 >NUL
+
+echo Process Completed 
+title Process Completed
+
+
+PING localhost -n 3 >NUL
+
+echo Running Programs
+title Running Programs
+
+PING localhost -n 1 >NUL
+
+start chrome.exe www.familyholiday.in.th
+
+
+
+
+
+
+
+
+
